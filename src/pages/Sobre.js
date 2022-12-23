43ecccff57415006b9f6e62f144d018ac9230d64
Jhonatan Almeida Alflen <110558256+JhoAlmeida@users.noreply.github.com>
2022-12-23 16:27:11 -0300

    Add files via upload

--- src/pages/Sobre.js
+++ src/pages/Sobre.js
@@ -1,8 +1,17 @@
 
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import React from 'react'
 import fotojho from "../assets/images/fotojho.png"
-import { ClassNames } from "@emotion/react";
-export default () => <img src={fotojho}/>;
 
 
-<a target="_blank" href="https://www.linkedin.com/in/jhonatan-almeida-alflen-965584248/">< texto do botao></texto> </a>
+export default function Sobre() {
+  return (
+    <>
+    <img className="img" src={fotojho} />
+    <a target="_blank" href="https://www.linkedin.com/in/jhonatan-almeida-alflen-965584248/"><LinkedInIcon sx={{ fontSize: '32px' }} /></a>
+  </>
+    )
+}
+
+
+
